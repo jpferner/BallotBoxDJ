d356f7127420cf80cedccb16a3abdb2f327f0d4b
Sam <sam@Sams-MacBook-Pro.local>
2021-11-20 10:17:37 -0500

    Create XCode project

---
+++ DJApp/DJApp/DJAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DJAppApp.swift
+//  DJApp
+//
+//  Created by Sam on 11/20/21.
+//
+
+import SwiftUI
+
+@main
+struct DJAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
